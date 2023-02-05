e7d75060b2728d0b72d75f6525025d7ee857dcf9
Bektemur Mamashaev <104037405+bektemur-07@users.noreply.github.com>
2023-02-05 15:43:33 +0500

    work with UI

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var totalBill: UITextField!
+    
+    @IBOutlet var tipAmount: UILabel!
+    @IBOutlet var tipSlider: UISlider!
+    
+    @IBOutlet var peopleAmount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tipAmount.text = String(format: "%.0f", tipSlider.value)
+        
     }
-
+    
+    @IBAction func tipSliderChanged(_ sender: UISlider) {
+        tipAmount.text = String(format: "%.0f", sender.value)
+    }
+    
 
 }
 
